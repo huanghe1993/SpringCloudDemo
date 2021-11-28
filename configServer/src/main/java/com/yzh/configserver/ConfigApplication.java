23d6a404930bc455b50c7facdb46b6bf192f1219
yuanzhihao <742599783@qq.com>
2021-11-28 15:17:05 +0800

    添加spring cloud config支持https代码

--- configServer/src/main/java/com/yzh/configserver/ConfigApplication.java
+++ configServer/src/main/java/com/yzh/configserver/ConfigApplication.java
@@ -1,8 +1,12 @@
 package com.yzh.configserver;
 
+import com.netflix.discovery.DiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+
+import javax.net.ssl.SSLContext;
 
 /**
  * @author yuanzhihao
@@ -11,6 +15,14 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigApplication {
+
+    // 通过https注册到eureka
+    @Bean
+    public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs(SSLContext sslContext) {
+        DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs = new DiscoveryClient.DiscoveryClientOptionalArgs();
+        discoveryClientOptionalArgs.setSSLContext(sslContext);
+        return discoveryClientOptionalArgs;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
